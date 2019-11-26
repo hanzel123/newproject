3e3067985315f5f86fa9653687078ff14e02679b
hanzel123 <45750027+hanzel123@users.noreply.github.com>
2019-11-25 21:53:08 -0800

    Add files via upload

---
+++ project/studentlogin.php
@@ -0,0 +1,32 @@
+<?php include('server.php') ?>
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Registration system PHP and MySQL</title>
+  <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<style type="text/css">
+
+
+  form, .content {
+    width: 30%;
+    margin: 0px auto;
+    padding: 20px;
+    border: 1px solid #b0c4de40;
+    background: #ffffff80;
+    border-radius: 0px 0px 10px 10px;
+}
+</style>
+<body>
+  <div class="header">
+  <br>
+  <h2 style="color:white;">Log in as</h2>
+
+
+  </div>
+  <center>
+  <a href="login1.php"> <img src="student.jpg" style="width:200px; padding:150px;" ><br></a>
+  <a href="login.php"> <img src="teacher.jfif"><a>
+</center>
+</body>
+</html>
